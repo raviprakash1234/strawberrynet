ce42b9bd73bbd0d8904a505a66e9fc8b2d6e9188
Gajraj Shekhawat <Gaju2gajraj@gmail.com>
2021-10-01 23:18:32 +0530

    brand filter shows by alfa, discount price changed search bar chaned

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connect = () =>{
+const connect = () => {
     return mongoose.connect("mongodb+srv://gourav:straw_berry_net@cluster0.aaoit.mongodb.net/strawberrynet?retryWrites=true&w=majority")
 }
 
